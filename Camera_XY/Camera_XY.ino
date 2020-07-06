f9cca0602b958d7c503e6913984a36ef40b85f09
GioByte10 <gvanni.bernal10@gmail.com>
2020-07-05 21:34:40 -0600

    Arduino

--- Camera_XY/Camera_XY.ino
+++ Camera_XY/Camera_XY.ino
@@ -2,16 +2,17 @@
 #include <Servo.h>
 #include <math.h>
 
-SoftwareSerial Bluet (11, 10);   //Tx Rx
+SoftwareSerial Bluet (10, 11);   //Tx Rx
 Servo servoX, servoY;
+
 // 316, 464
-const int numC = 100;
+
+const int maxX = 316, maxY = 464;
+const double range = 90 * (M_PI/180);
 
 String aux = "";
 unsigned long time;
 
-int x[numC];
-int y[numC];
 int turn = 1;
 
 double xc, yc, toWriteX, toWriteY;
@@ -28,21 +29,23 @@ void setup() {
   delay(500);
   Bluet.begin(38400);
   reset();
-  
-  servoX.write(90);
-  servoY.write(90);
+
+  servoX.attach(5);
+  servoY.attach(6);
   
 }
 
 void loop() {
 
   if (Bluet.available() > 0){
+    once = false;
     Serial.println("Receiving Bytes...");
     
     time = millis();
 
     while (Bluet.available() > 0){
       c = char(Bluet.read());
+      //Serial.print(c);
       if(!(c == '(' || c == ')')){
         if(!(c == ' ')){
           aux += c;
@@ -50,22 +53,21 @@ void loop() {
         }else{
           
           if(turn == 1){
-            Serial.println(aux);
-            x[n] = aux.toInt();
             xc = aux.toInt();
-            toWriteX = 90 + (atan((xc - 158) / 381.445) * (180 / M_PI));
+            toWriteX = 90 + (atan((xc - maxX/2) / ((maxX/2)/tan(range/2))) * (180 / M_PI));
+            Serial.print(toWriteX);
+            Serial.print("     ");
             aux = "";
             turn++;
             
         }else if(turn == 2){
-            y[n] = aux.toInt();
             yc = aux.toInt();
-            toWriteY = 90 + (atan((yc - 232) / 560.097) * (180 / M_PI));
+            toWriteY = 80 + (atan((yc - maxY/2) / ((maxY/2)/tan(range/2))) * (180 / M_PI));
+            Serial.println(toWriteY);
             aux = "";
             turn = 1;
-            n++;
             servoX.write(toWriteX);
-            servoY.write(toWriteY);
+            servoY.write(toRDeg(toWriteY));
         }
         }
       }
@@ -73,39 +75,25 @@ void loop() {
     }
   }
 
-  if(millis() - time > 5000){                              //Giving a 5 seconds timeout for the Bluetooth "packages"
-    n = 0;
-    if(x[0] >= 0){
+  if(millis() - time > 1000){                              //Giving a 5 seconds timeout for the Bluetooth "packages"
 
-      showCoords();
+    if(!once){
 
-      delay(5000);
+      delay(1000);
       reset();
+      once = true;
      
     }
   }
 }
 
 void reset(){
-  for(int i = 0; i < numC; i++){
-    x[i] = -1;
-    y[i] = -1;
-  }
+
+  servoX.write(90);
+  servoY.write(toRDeg(80));
+  
 }
 
-
-void showCoords(){
-
-  for(int i = 0; i < numC; i++){
-
-    Serial.print("X[");
-    Serial.print(i);
-    Serial.print("]: ");
-    Serial.print(x[i]);
-    Serial.print("   Y[");
-    Serial.print(i);
-    Serial.print("]: ");
-    Serial.println(y[i]);
-    
-  } 
+double toRDeg(double des){
+  return des / 1.15;
 }
